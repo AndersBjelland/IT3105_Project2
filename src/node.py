638511fd38df4c28aa5fbce801487c5139213bce
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-13 13:28:17 +0100

    get child method

--- src/node.py
+++ src/node.py
@@ -1,12 +1,16 @@
 
 class Node():
-    def __init__(self, environment=None, parent=None):
+    def __init__(self, environment=None, parent=None, action=None):
+        """
+        action is the action taken by the parent to get to this node
+        """
         self.environment = environment
         self.set_parent(parent)
         self.children = []
-        self.q_values = []
-        self.traverse_count = 0
-        self.actions = []
+        self.q_values = {} # {action:q_value}
+        self.traverse_count = 0 # Number of the edge from parent to this node has been traversed
+        self.action = action
+        self.state_value = 0
 
     def get_parent(self):
         return self.parent
@@ -22,7 +26,11 @@ class Node():
     def add_child(self, child):
         self.children.append(child)
 
-    def add_action(self, action):
-        self.actions.append(action)
+    def get_child(self, action) -> 'Node':
+        for child in self.get_children():
+            if child.action == action:
+                return child
+        return None
+
     
         
