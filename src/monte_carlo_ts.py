dc573fccf9b17a33aba573a73db724e1e1cfc8d7
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-13 09:33:42 +0100

    changed _is_leaf_node

--- src/monte_carlo_ts.py
+++ src/monte_carlo_ts.py
@@ -39,19 +39,15 @@ class MTCS():
         A node is defined as a leaf node if it has a potential
         child from which no simulation (rollout) has yet been initiated
         """
-        env = node.environment
-        available_actions = env.available_actions()
-        
-        # To aviod checking if every available action is represented in any of the children 
-        # Consider whether it is safe to conclude that a node is not a leaf if it has been traversed > number of available actions
-        #we check if this node has been traversed > available moves. If it has, we know it is not a leaf
-        
-        for action in available_actions:
-            env.make_action(action)
-            for child in node.get_children():
-                if env.get_state() == child.environment.get_state():
-                    return False
+        available_actions = node.environment.available_actions()
+
+        # If the number of children equals the number of available actions we can conclude that it is not a leaf node
+        if len(available_actions) == len(node.get_children()):
+            return False
+
         return True
+        
+
                     
     
 
