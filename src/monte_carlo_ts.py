ebfc468db7a9e643f3cd31e0da96f0c910f325b8
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-13 15:50:38 +0100

    fixed bugs

--- src/monte_carlo_ts.py
+++ src/monte_carlo_ts.py
@@ -3,6 +3,7 @@ from node import Node
 
 from typing import Callable, List, Tuple
 import numpy as np
+import random
 
 """
 This class contains code to perform a Monte Carlo Tree Search (MCTS).
@@ -13,7 +14,7 @@ We will in this project use the actor network as the target policy.
 
 class MTCS():
 
-    def __init__(self, target_policy, environment: Hex, exploration_bonus='utc', c=1):
+    def __init__(self, target_policy, environment: Hex, exploration_bonus='uct', c=1):
         if exploration_bonus=='uct':
             self.exploration_bonus = lambda s,a: self._utc(s,a,c)
         else:
@@ -22,40 +23,41 @@ class MTCS():
         self.environment = environment
         self.root = Node(environment=environment)
 
-    def traverse_to_leaf(self) -> List[Node]:
+    def _traverse_to_leaf(self) -> Node:
         current = self.root
-        path = []
+        #path = []
         while not self._is_leaf_node(current):
-            path.append(current)
+            #path.append(current)
             # Use the policy to get the next node and set it to current
             current = self.tree_policy(current)
         # Add the leaf node to path
-        path.append(current)
-        return path
+        #path.append(current)
+        return current
 
-    def expand(self, node: Node) -> Node: 
+    def _expand(self, node: Node) -> Node: 
         # Check if the node represents a final state
         if node.environment.get_winner():
             return node
 
         env = node.environment.copy()
-        actions = [action for action in env.available_actions() if action not in node.actions]
-        action = np.random.choice(actions)
+        actions = [action for action in env.available_actions() if node.get_child(action) is None]
+        action = random.choice(actions)
         env.make_action(action)
 
         return Node(environment=env, parent=node, action=action)
 
-    def rollout(self, node: Node) -> int:
+    def _rollout(self, node: Node) -> int:
         env = node.environment.copy()
         while not env.get_winner():
-            action = self.target_policy.get_action(env.get_state())
+           # action = self.target_policy.get_action(env.get_state())
+            action = self.target_policy.get_action(env)
             env.make_action(action)
         
         winner = env.get_winner()
-        return 1 if self.root.environment.current == winner else -1
+        return 1 if self.root.environment.current_player == winner else -1
             
 
-    def back_prop(self, node: Node, value: float):
+    def _back_prop(self, node: Node, value: float):
         "starts by updating q-value of the edge between the given node and the parent node - check if correct"
         current = node
 
@@ -66,7 +68,10 @@ class MTCS():
 
             parent.q_values[current.action] = value if current.action not in parent.q_values else (parent.state_value + value)/current.traverse_count
             
-            current = node.parent
+            current = current.parent
+
+        current.traverse_count += 1
+        
 
 
     def _is_leaf_node(self, node) -> bool:
@@ -84,7 +89,7 @@ class MTCS():
 
     def _utc(self, node: Node, action: Tuple[int,int], c: float) -> float:
         visits_to_node = node.traverse_count
-        child = node.get_children(action)
+        child = node.get_child(action)
         performed_action_count = child.traverse_count
 
         return c*np.sqrt(np.log(visits_to_node)/(1+performed_action_count))
@@ -95,12 +100,38 @@ class MTCS():
         """
         available_actions = node.environment.available_actions()
         q_and_u_values = {action : (node.q_values[action], self.exploration_bonus(node, action)) for action in available_actions}
-        if node.environment.current() == self.root.environment.current():
+        if node.environment.current_player == self.root.environment.current_player:
             # return argmax q+u
             return node.get_child(max(q_and_u_values.keys(), key=lambda x : sum(q_and_u_values[x])))
 
         # return argmin q-u
-        return node.get_child(min(q_and_u_values.keys(), key=lambda x : q_and_u_values[0] - q_and_u_values[1]))
+        return node.get_child(min(q_and_u_values.keys(), key=lambda x : q_and_u_values[x][0] - q_and_u_values[x][1]))
+
+
+    def perform_simulation(self):
+
+        leaf_node = self._traverse_to_leaf()
+        expanded_node = self._expand(leaf_node)
+        value = self._rollout(expanded_node)
+        self._back_prop(expanded_node, value)
+
+    def search(self, n_simulations: int) -> Tuple[int, int]:
+        for _ in range(n_simulations):
+            self.perform_simulation()
+        return (max(self.root.get_children(), key=lambda x : x.traverse_count)).action
+
+
+class test_target_policy:
+
+    def get_action(self, env):
+        return random.choice(env.available_actions())
+
+
+hex = Hex((3,3))
+monte = MTCS(test_target_policy(), hex)
+print(monte.search(1000))
+
+    
         
 
 
