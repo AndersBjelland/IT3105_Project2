aa2ce563ef14903324ee38b6aac240da5bc3fabd
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-16 13:23:53 +0100

    padding functionality

--- src/encoder.py
+++ src/encoder.py
@@ -1,3 +1,4 @@
+from .hex import Hex
 import abc
 import numpy as np
 import tensorflow as tf
@@ -15,27 +16,32 @@ class Encoder(metaclass = abc.ABCMeta):
 
 class HexEncoder(Encoder):
 
-
-    def encode(self, piece_owner: int, padding: int) -> 'tensor':
+    def encode(self, piece_owner: int, padding=0) -> 'tensor':
         """
         Returns a tensor that will serve as one feature/encoding of a Hex board. 
         The dimension of the tensor will be (1, row_size, column_size, n_planes)
 
         The padding parrameters specifies how much the planes will be padded.
-        Each plane encoder pads in a plane specific way.
+        
         """
-        blue = self.player_stones_encoding(1)
-        red = self.player_stones_encoding(2)
-        empty = self.player_stones_encoding(0)
 
-        blue_bridge = self.bridge_encoding(1, plane_type='endpoints')
-        red_bridge = self.bridge_encoding(2, plane_type='endpoints')
+        # Alter the environment according to the padding parameter.
+        # We add 'padding' layers of cells outside the current board and set the owner of those cells
+        # according to the player that owns that side.
+        env = self.create_padded_env(padding=padding)
 
-        to_play_blue = self.to_play_endocing(1)
-        to_play_red = self.to_play_endocing(2)
+        blue = self.player_stones_encoding(1, env=env)
+        red = self.player_stones_encoding(2, env=env)
+        empty = self.player_stones_encoding(0, env=env)
 
-        to_play_save_bridge = self.bridge_encoding(self.env.current_player, plane_type='save')
-        to_play_form_bridge = self.bridge_encoding(self.env.current_player, plane_type='form')
+        blue_bridge = self.bridge_encoding(1, plane_type='endpoints', env=env)
+        red_bridge = self.bridge_encoding(2, plane_type='endpoints', env=env)
+
+        to_play_blue = self.to_play_endocing(1, env=env)
+        to_play_red = self.to_play_endocing(2, env=env)
+
+        to_play_save_bridge = self.bridge_encoding(self.env.current_player, plane_type='save', env=env)
+        to_play_form_bridge = self.bridge_encoding(self.env.current_player, plane_type='form', env=env)
 
         planes = [blue, red, empty, blue_bridge, red_bridge, to_play_blue, to_play_red, to_play_save_bridge, to_play_form_bridge]
         # Create new axis in all planes
@@ -46,6 +52,32 @@ class HexEncoder(Encoder):
         feat = np.expand_dims(feat, axis=0)
         # Return as tensor
         return tf.convert_to_tensor(feat)
+
+    def create_padded_env(self, padding:int) -> 'env':
+        current_env_size = self.env.size
+        new_size = (current_env_size[0] + padding+1, current_env_size[1] + padding+1)
+        coordinate_scaler = lambda x: (x[0] + padding, x[1] + padding) # coresponding coordinate in new env
+
+        new_env = Hex(new_size, self.env.start_player)
+
+        # Iterate of cells in old env and set corresponding cells in new
+        for cell in self.env.get_board().get_cells():
+            coordinate = (cell.get_row(), cell.get_column())
+            coordinate = coordinate_scaler(coordinate)
+            new_env.set_piece(coordinate, cell.get_piece())
+
+        # Place stones in the added layers
+        for layer in range(padding):
+            NW, SE = new_env.get_NW_coordinates(layer=layer), new_env.get_SE_coordinates(layer=layer)
+            NE, SW = new_env.get_NE_coordinates(layer=layer), new_env.get_SW_coordinates(layer=layer)
+
+            for coordinate in NE + SW:
+                new_env.set_piece(coordinate, 2)
+
+            for coordinate in NW + SE:
+                new_env.set_piece(coordinate, 1)
+
+        return new_env  
 
     def get_bridge_end_points(self, row: int, column: int) -> List[Tuple[int, int]]:
         return [(row-2, column+1), (row-1, column+2), 
@@ -78,7 +110,7 @@ class HexEncoder(Encoder):
         
         return [(row1,col1), (row2, col2)]
 
-    def player_stones_encoding(self, piece_owner: int) -> np.ndarray:
+    def player_stones_encoding(self, piece_owner: int, env: Hex) -> np.ndarray:
         """
         piece_owner is an int indicating which plane to return, with respect to EMPTY, BLUE, RED
         Returns a plane of the board with entry 1 if piece owner owns that piece, 0 otherwise
@@ -91,23 +123,23 @@ class HexEncoder(Encoder):
                             ]               
         
         """
-        state = self.env.get_state()
+        state = env.get_state()
 
         # Slice state because current player is first element of state
         feat = [1 if piece==piece_owner else 0 for piece in state[1:]]
-        feat = np.array(feat).reshape(self.env.get_size())
+        feat = np.array(feat).reshape(env.get_size())
         return feat
 
-    def to_play_endocing(self, player: int) -> np.ndarray:
+    def to_play_endocing(self, player: int, env: Hex) -> np.ndarray:
         """
         Returns a plane with all 1 if the current player and the player argument are equal, all 0 otherwise.
         """
 
-        feat = [[1 if self.env.current_player == player else 0 for _ in range(self.env.size[1])] for _ in range(self.env.size[0])]
+        feat = [[1 if env.current_player == player else 0 for _ in range(env.size[1])] for _ in range(env.size[0])]
         return np.array(feat)
 
 
-    def bridge_encoding(self, piece_owner: int, plane_type: str) -> np.ndarray:
+    def bridge_encoding(self, piece_owner: int, plane_type: str, env: Hex) -> np.ndarray:
         """
         Bridge encoding takes a plane_type argument that cause the method to return the following planes.
 
@@ -116,11 +148,11 @@ class HexEncoder(Encoder):
         'endpoints': An encoding taht sets 1 if that cell forms a bridge.
         """
 
-        feat = [[0 for _ in range(self.env.size[1])] for _ in range(self.env.size[0])]
-        for row in range(self.env.size[0]):
-            for column in range(self.env.size[1]):
+        feat = [[0 for _ in range(env.size[1])] for _ in range(env.size[0])]
+        for row in range(env.size[0]):
+            for column in range(env.size[1]):
 
-                piece = self.env.get_board().get_cell(row, column).get_piece()
+                piece = env.get_board().get_cell(row, column).get_piece()
                 
                 # if the piece is not owned by the piece_owner we move to next
                 if piece != piece_owner:
@@ -133,7 +165,7 @@ class HexEncoder(Encoder):
                 for bridge_row, bridge_column in end_points:
                     
                     # Cell of the current endpoint
-                    bridge_cell = self.env.get_board().get_cell(bridge_row, bridge_column)
+                    bridge_cell = env.get_board().get_cell(bridge_row, bridge_column)
                     
                     # If the bridge cell is None we know we are 'outside' the board
                     if bridge_cell is None:
@@ -145,7 +177,7 @@ class HexEncoder(Encoder):
                         # Check if the endpoint forms a bridge together with the current cell
                         if bridge_cell.get_piece() != piece_owner:
                             continue
-                        feat = self._process_save_endpoint(feat, bridge_row, bridge_column, row, column, piece_owner)
+                        feat = self._process_save_endpoint(feat, bridge_row, bridge_column, row, column, piece_owner, env)
 
                     # Process as a form point
                     elif plane_type == 'form':
@@ -160,18 +192,19 @@ class HexEncoder(Encoder):
 
         return np.array(feat)       
 
-    def _process_save_endpoint(self, feat, bridge_row: int, bridge_column: int, row: int, column: int, piece_owner: int) -> List[List[int]]:
+    def _process_save_endpoint(self, feat, bridge_row: int, bridge_column: int, row: int, column: int, piece_owner: int, env: Hex) -> List[List[int]]:
         carrier_point1, carrier_point2 = self.get_carrier_points((row, column), (bridge_row, bridge_column))
                     
-        carrier_point1_piece = self.env.get_board().get_cell(carrier_point1[0], carrier_point1[1]).get_piece() 
-        carrier_point2_piece = self.env.get_board().get_cell(carrier_point2[0], carrier_point2[1]).get_piece()
+        carrier_point1_piece = env.get_board().get_cell(carrier_point1[0], carrier_point1[1]).get_piece() 
+        carrier_point2_piece = env.get_board().get_cell(carrier_point2[0], carrier_point2[1]).get_piece()
 
-        if carrier_point2_piece != 0 and carrier_point2_piece != piece_owner:
+        
+        if carrier_point2_piece != 0 and carrier_point2_piece != piece_owner and carrier_point1_piece==0:
             feat[carrier_point1[0]][carrier_point1[1]] = 1
         else:
             feat[carrier_point1[0]][carrier_point1[1]] += 0
 
-        if carrier_point1_piece != 0 and carrier_point1_piece != piece_owner:
+        if carrier_point1_piece != 0 and carrier_point1_piece != piece_owner and carrier_point1_piece==0:
             feat[carrier_point2[0]][carrier_point2[1]] = 1
         else:
             feat[carrier_point2[0]][carrier_point2[1]] += 0
