82556005c38067f2381c6277b2a3b67213024834
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-17 19:13:11 +0100

    minor

--- src/agent.py
+++ src/agent.py
@@ -2,6 +2,7 @@ from .actor import Actor
 from .monte_carlo_ts import MCTS
 from .hex import Hex
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Agent:
@@ -15,10 +16,12 @@ class Agent:
         replay_buffer = []
 
         while env.get_winner() == 0:
-            distribution = self.mcts.search(n_simulations=100)
-            
+            distribution = self.mcts.search(n_simulations=1000)
+            self.plot_distribution(distribution)
             # Choose action with probability proportional to the traverse count
+            
             action = list(distribution.keys())[np.random.choice([_ for _ in range(len(distribution.keys()))], p=list(distribution.values()))]
+            print("Chosen action is {}".format(action))
             replay_buffer.append((env, distribution))
             
             env.make_action(action)
@@ -28,7 +31,11 @@ class Agent:
         print("winner:", env.get_winner())
         return replay_buffer, env
 
-    
+    def plot_distribution(self, distribution):
+        print(distribution)
+        plt.figure(figsize=(10,5))
+        plt.bar(x=[_ + 1 for _ in range(len(distribution))], height=distribution.values(), tick_label=[str(_) for _ in distribution.keys()])
+        plt.show()
 
 
 
