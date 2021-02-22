6535d2dcaafb451abde4c1d1b6d2a3d4f9f054b9
Anders Bjelland <andertb@stud.ntnu.no>
2021-02-22 08:50:35 +0100

    add imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from .hexagonal_grid import *
 from .hex import *
+from .nim import *
 from .node import *
 from .monte_carlo_ts import *
 from .encoder import *
 from .actor import *
 from .agent import *
+
