c07b4d4d775c2cede72996e69c62dc1f1401c1d6
Anders Bjelland <andertb@stud.ntnu.no>
2021-03-27 15:26:05 +0100

    working on training of critic

--- src/agent.py
+++ src/agent.py
@@ -1,12 +1,14 @@
 from .actor import Actor
 from .critic import Critic
 from .monte_carlo_ts import MCTS
+from .encoder import HexEncoder
 from .hex import Hex
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from copy import deepcopy
 import random
+import tensorflow as tf
 
 import math
 
@@ -40,13 +42,14 @@ class Agent:
         # include a label in each element of the replay buffer with the outcome for critic training
         winner = env.get_winner()
         for i in range(len(replay_buffer)):
-            replay_buffer[i] += (1,) if replay_buffer[i][0].get_current_player() == winner else (-1,)
+            replay_buffer[i] += (1,) if replay_buffer[i][0].get_current_player() == winner else (0,)
         
         env.reset()
         
         return replay_buffer
 
     def train_agent(self, env: Hex, n_episodes:int, n_simulations: int, rollout_start_prob:float, rollout_end_prob:float, epochs=1, M=1, file_path=''):
+        
 
         replay_buffer = []
         epsilon_decay_factor = (self.actor.epsilon - self.actor.end_epsilon)/n_episodes
@@ -59,11 +62,28 @@ class Agent:
             replay_buffer = self.run_episode(env=env, n_simulations=n_simulations, rollout_prob=rollout_prob) + replay_buffer
             # Only keep the last 5000 steps
             replay_buffer = replay_buffer[:500]
+            if (i == 5):
+                return replay_buffer
             # Train network for actor and critic
             print("--------------Actor training--------------")
             self.actor.end_of_episode(replay_buffer, epochs=epochs)
             print("--------------Critic training-------------")
             self.critic.end_of_episode(replay_buffer, epochs=epochs)
+            print("--------------Critic2 training------------")
+            encoder = HexEncoder(padding=2)
+            critic2 = Critic(learning_rate=0.001,
+                nn_shape=(155,128,1),
+                filters=(128,),
+                kernel_sizes=((3,3),),
+                    nn_opt='Adam',
+                nn_activation='relu',
+                nn_last_activation='sigmoid',
+                nn_loss= 'mse',
+                encoder=encoder
+                )
+            x = tf.concat([encoder.encode(entry[0]) for entry in replay_buffer], 0)  
+            y = tf.convert_to_tensor([entry[2] for entry in replay_buffer])
+            critic2.model.fit(x,y, epochs=epochs)
             # Update epsilon
             self.actor.epsilon = self.actor.epsilon - epsilon_decay_factor*(i+1)
              # Reduce prob of rollout
